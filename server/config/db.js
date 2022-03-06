ae5ff4efcd1b12af5ba993e68e52c4c9ce4a6701
Raja reddy <rajkumarreddybanala@gmail.com>
2022-03-05 21:24:31 -0500

    intialcommit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  
+  "URI": "mongodb+srv://rajkumarreddy:dUnUKPGINB1P0TaS@mongodbserver.ylkle.mongodb.net/book_store?retryWrites=true&w=majority"
+  
+};
